967cfbefcb76edc4902c7f675f5259491eaf3045
maryhuziuk <116351558+maryhuziuk@users.noreply.github.com>
2023-04-24 12:05:34 +0300

    check

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,4 +1,4 @@
-// import flatpickr from 'flatpickr';
+import flatpickr from 'flatpickr';
 // import '/flatpickr/dist/flatpickr.min.css';
 // import { Notify } from '/notiflix/build/notiflix-notify-aio';
 
